42cedb9c068d575de2573f3d4c947b561e57f126
DI <cdi122@nate.com>
2022-08-05 16:41:24 +0900

    실제로 저장 / 저장된 값 조회 가능

--- app/src/main/java/com/example/sharedpreference_dain/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreference_dain/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.sharedpreference_dain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import com.example.sharedpreference_dain.utils.ContextUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        autoLoginCheckBox.setOnCheckedChangeListener { button, isChecked ->  }
+        ContextUtil.setAutoLogin(this, isChecked)
+
+        }
+    autoLoginCheckBox.ischecked = ContextUtil.getAutoLogin(this)
     }
-}
+ }
